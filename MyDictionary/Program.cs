85fa256cd4d8535bde43effbf3f68607c968b368
Samet HANCI <Samet HANCI@SAMET>
2022-12-13 19:27:43 +0300

    Ödev 5

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string> dictionary = new Dictionary<string>();
+            dictionary.Add("Samet");
+            dictionary.Add("Ahmet");
+            Console.WriteLine(dictionary.Length);
+            foreach (var isim in dictionary.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
